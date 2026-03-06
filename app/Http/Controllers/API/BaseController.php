94aef325a2277ff4bd8230f1777beaf4e336581a
alimasudd <alimasudd.dev@gmail.com>
2026-03-06 10:40:56 +0700

    first commit

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @param $result
+     * @param $message
+     * @return JsonResponse
+     */
+    public function sendResponse($result, $message): JsonResponse
+    {
+        $response = [
+            'success' => true,
+            'message' => $message,
+            'data'    => $result,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * return error response.
+     *
+     * @param $error
+     * @param array $errorMessages
+     * @param int $code
+     * @return JsonResponse
+     */
+    public function sendError($error, array $errorMessages = [], int $code = 404): JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
