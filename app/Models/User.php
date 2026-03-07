d13297298432393710135184d778d3e30c21fddd
alimasudd <alimasudd.dev@gmail.com>
2026-03-07 18:31:48 +0700

    feat: Add User model with custom table `m_user`, fillable attributes, and password hashing.

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiables;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = 'm_user';
     public $timestamps = false;
